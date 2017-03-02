8fb29a8d2314adab865c2dc407ef089530848106
Meryl Dakin <meryl.k.dakin@gmail.com>
2017-03-02 22:03:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  end
+  arr_to_check = (2..int-1).to_a
+  for num in arr_to_check
+    if int % num == 0
+      return false
+    end
+  end
+  return true
+
+end
